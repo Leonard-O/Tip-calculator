2d494a2debdd7e93c15fdfef8f5a5de21d92e1cb
Leonard <realcodedevs@gmail.com>
2022-02-17 20:49:30 +0300

    mobile version

---
+++ main.js
@@ -0,0 +1,19 @@
+// const body = document.querySelector('body');
+// const noOfPeople = document.querySelector('#people').value;
+// const hide = document.querySelector('.hide');
+
+// // body.style.backgroundColor = 'red';
+
+// noOfPeople.value.style.color = 'red';
+// noOfPeople.value.style.textAlign = 'left';
+
+// let input2 = 0;
+
+// noOfPeople.addEventListener('click', () => {
+//     input2 = noOfPeople.value;
+
+//     console.log(input2);
+
+//     hide.style.opacity = '1';
+//     hide.style.color = 'red';
+// });
